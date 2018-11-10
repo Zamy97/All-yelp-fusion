df477e923dc8e96befca92bc4ae177d37d945b1c
Akhtar Zaman <auz.zamy@gmail.com>
2018-11-09 16:17:27 -0800

    gotten rid of few files

--- fusion/node/sample.js
+++ fusion/node/sample.js
@@ -4,7 +4,7 @@ const yelp = require('yelp-fusion');
 
 // Place holder for Yelp Fusion's API Key. Grab them
 // from https://www.yelp.com/developers/v3/manage_app
-const apiKey = 'Ttm9T3jW7ItGLfoEF1IaUztpCbCc5BVHFgzNjAbKgz2iyEP9JTBsjYJFxYz-HBux6r4GGROqZb8AWno1CvxmretUb7l7L79FUOByZGk9s2SX9kWEXF2X3BNPcuPlW3Yx';
+const apiKey = '';
 
 const searchRequest = {
   term:'Burrito',
