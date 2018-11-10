8174f58348a93c3ca47a7e01fc23424a5ca796af
Akhtar Zaman <auz.zamy@gmail.com>
2018-11-09 16:11:47 -0800

    testing to see if the new url is working

--- fusion/node/sample.js
+++ fusion/node/sample.js
@@ -4,11 +4,11 @@ const yelp = require('yelp-fusion');
 
 // Place holder for Yelp Fusion's API Key. Grab them
 // from https://www.yelp.com/developers/v3/manage_app
-const apiKey = '<YOUR_API_KEY>';
+const apiKey = 'Ttm9T3jW7ItGLfoEF1IaUztpCbCc5BVHFgzNjAbKgz2iyEP9JTBsjYJFxYz-HBux6r4GGROqZb8AWno1CvxmretUb7l7L79FUOByZGk9s2SX9kWEXF2X3BNPcuPlW3Yx';
 
 const searchRequest = {
-  term:'Four Barrel Coffee',
-  location: 'san francisco, ca'
+  term:'Burrito',
+  location: 'Near UC Berkeley',
 };
 
 const client = yelp.client(apiKey);
